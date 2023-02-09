b72f4d613ae2ddca11711fe17f3177d90f97db09
subramanian42 <88108372+subramanian42@users.noreply.github.com>
2023-02-09 20:26:34 +0530

    feat: add weather bloc provider globally

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/pages/homepage.dart';
-//import 'package:weather_app/pages/homepage.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/business/WeatherBloc.dart';
+
+import 'package:weather_app/presentation/screens/homepage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+    return BlocProvider<WeatherBloc>(
+      create: (context) => WeatherBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
     );
   }
 }
